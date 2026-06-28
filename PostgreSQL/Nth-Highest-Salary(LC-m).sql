3fa9b6d396cd7a8304f4039da9171d1b13a8af25
Abdulsalim123 <abdulsalim432005@gmail.com>
2026-06-28 21:41:42 +0530

    SQL (LeetCode - Medium) | Written a solution to find the nth highest distinct salary from the Employee table.

---
+++ PostgreSQL/Nth-Highest-Salary(LC-m).sql
@@ -0,0 +1,69 @@
+/*
+Table: Employee
+
++-------------+------+
+| Column Name | Type |
++-------------+------+
+| id          | int  |
+| salary      | int  |
++-------------+------+
+id is the primary key (column with unique values) for this table.
+Each row of this table contains information about the salary of an employee.
+ 
+
+Write a solution to find the nth highest distinct salary from the Employee table. If there are less than n distinct salaries, return null.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
+| 2  | 200    |
+| 3  | 300    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| 200                    |
++------------------------+
+Example 2:
+
+Input: 
+Employee table:
++----+--------+
+| id | salary |
++----+--------+
+| 1  | 100    |
++----+--------+
+n = 2
+Output: 
++------------------------+
+| getNthHighestSalary(2) |
++------------------------+
+| null                   |
++------------------------+
+*/
+
+CREATE OR REPLACE FUNCTION NthHighestSalary(N INT) RETURNS TABLE (Salary INT) AS $$
+BEGIN
+  RETURN QUERY (
+    -- Write your PostgreSQL query statement below.
+    SELECT DISTINCT ranking.salary
+    FROM (
+        SELECT Employee.salary, 
+               DENSE_RANK() OVER (ORDER BY Employee.salary DESC) as rnk
+        FROM Employee
+    ) ranking
+    WHERE ranking.rnk = N
+  );
+END;
+$$ LANGUAGE plpgsql;
